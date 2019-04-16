27b7120d2aadc5f69a2a1d241d2907f761203d99
Jamie Barton <hi@jamiebarton.co.uk>
2019-04-16 23:01:27 +0100

    remove @babel/plugin-transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  presets: [['@babel/env'], '@babel/react'],
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/react'
+  ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     [
-      '@babel/plugin-transform-runtime',
-      {
-        regenerator: true
-      }
-    ],
-    [
       'babel-plugin-styled-components',
       {
-        ssr: false,
-        displayName: false,
-        fileName: false,
-        pure: true
+        ssr: false
       }
     ]
   ]
