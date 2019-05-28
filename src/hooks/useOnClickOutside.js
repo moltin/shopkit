4d0a64f42d78466d62bc3b074da28ba259283b39
Daniel Sokil <sokil@pop-os.localdomain>
2019-05-28 14:28:01 -0400

    Fix #3

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -3,11 +3,9 @@ import { useEffect } from 'react'
 export default function useOnClickOutside(ref, handler) {
   useEffect(() => {
     const listener = event => {
-      if (!ref.current || ref.current.contains(event.target)) {
-        return
+      if (event.target.classList.contains('shopkit-modal-overlay')) {
+        handler(event)
       }
-
-      handler(event)
     }
 
     document.addEventListener('mousedown', listener)
