5c959226427816bd11903aeaf90c25429a45f71e
Petro Dubno <petro.dubno@gmail.com>
2020-07-20 19:58:05 +0300

    Remove comments

--- src/model/index.js
+++ src/model/index.js
@@ -11,8 +11,6 @@ export default {
       await dispatch.cart.getCart(cartId)
       await dispatch.cart.setCartId(cartId)
       await dispatch.user.setCustomerId(customerId)
-      // await dispatch.user.setCustomerToken(customerToken)
-      // await dispatch.user.getCustomer({ customerId, customerToken })
     }
   ),
   modal,
